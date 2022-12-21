968c47fc4f9cf0c4cba5510cc267803c5bc77e53
ayushilakhotia <77203645+ayushilakhotia@users.noreply.github.com>
2022-12-21 12:07:22 +0530

    Update Player.js

--- js/Player.js
+++ js/Player.js
@@ -5,8 +5,8 @@ class Player {
     this.positionX = 0;
     this.positionY = 0;
     this.rank = 0;
-    this.fuel = 185;
-    this.score = 0;
+    //this.fuel = 185;
+    //this.score = 0;
   }
 
   addPlayer() {
